adaab1a2607f0a220eaf44278467c328ec945f94
Jacob Bednarz <jacob.bednarz@gmail.com>
2015-05-05 16:21:09 +1000

    Update base index.php

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,9 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
-	<body>
-		<h1>Welcome to Yet Another LAMP Server!</h1>
-		<?php
-			phpinfo();
-		?>
+
+<body>
+	<h1>Welcome to Yet Another LAMP Server!</h1>
+		<?php phpinfo(); ?>
 	</body>
 </html>
